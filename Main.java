91523abff0ae6804de21290f8f314ac0bb5ca297
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-19 08:55:51 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+// This program can't find its variable.
+
+public class Main
+{
+   public static void main(String[] args)
+   {
+      System.out.println(value);  // ERROR!
+      int value = 100;
+   }
+}
+
+
